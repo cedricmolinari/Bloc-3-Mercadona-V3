37dde9d40b66726f849b2198a7bb6b73e8b47b56
cedricm <cedric.molinari0@gmail.com>
2023-11-05 23:00:55 +0100

    Remove deleted files

--- src/main/java/com/mercadona/promotionmanagement/controller/UtilisateurController.java
+++
@@ -1,7 +0,0 @@
-package com.mercadona.promotionmanagement.controller;
-
-import org.springframework.stereotype.Controller;
-@Controller
-public class UtilisateurController {
-
-}
--- src/main/java/com/mercadona/promotionmanagement/core/repository/CategorieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mercadona.promotionmanagement.core.repository;
-
-import com.mercadona.promotionmanagement.core.entity.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
-}
-
